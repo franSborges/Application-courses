e50af3808641ffc6f67cb84ebb2b290f293877e9
franSborges <borgesfran988@gmail.com>
2022-06-24 14:28:41 -0300

    :feat: implements models courses

---
+++ src/create.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const result = await prisma.courses.create({
+    data: {
+      duration: 200,
+      name: "Curso de Elixir",
+      description: "Curso excelente de Elixir"
+    }
+  });
+  console.log(result)
+}
+
+main();
