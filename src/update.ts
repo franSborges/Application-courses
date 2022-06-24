ff67ebf1ca5baf096fbd3e637819294ab190564d
franSborges <borgesfran988@gmail.com>
2022-06-24 14:36:40 -0300

    :feat: implements function update

--- src/update.ts
+++ src/update.ts
@@ -3,10 +3,15 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const course = await prisma.courses.findFirst({
-    take: -1,
-  });
-  console.log(course)
+  const course = await prisma.courses.update({
+    where: {
+      id: "dd6c6348-48d9-4cba-bd26-2cb6a8b564bf"
+    },
+    data: {
+      duration: 300
+    }
+  })
+  console.log(course);
 }
 
 main();
